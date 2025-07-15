cef70b6af34d7a12371aa92f2b433f657e271328
HARISHIMSS <harishm@integramicro.com>
2025-07-15 22:38:45 +0530

    updated

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+        ...globals.jest, // 👈 this line enables Jest globals
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+    },
+    rules: {
+      ...ts.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: ['lib/**'],
+  },
+];
